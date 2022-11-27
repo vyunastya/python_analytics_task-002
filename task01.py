6cca3fcc169379558fc4b6f35363248a248fdece
Anastasia <vyugovaana@mail.ru>
2022-11-27 21:11:46 +0300

    1 rask done

--- task01.py
+++ task01.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+#1 Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+#Разделение целочисленой и дробной через запятую
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+number = input("Введите число ")
+sum = 0
+for i in number:
+    if i != ",":
+        sum += int(i)
+print (f'Сумма цифр числа {number} равна {sum}')
